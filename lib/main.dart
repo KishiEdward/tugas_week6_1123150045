d58e50bed851da748e1d98aeec63c07a6d5e555c
KishiEdward <dzidanrh@gmail.com>
2025-10-31 19:42:46 +0700

    update menambahkan material3 true

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "NIM : 1123150045, Dzidan Rafi H",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      home: const Splashscreen1(),
+    );
   }
 }
