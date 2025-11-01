3f1d8252a7f2ca8c8646d435efe7b6958ce95b27
KishiEdward <dzidanrh@gmail.com>
2025-11-01 09:53:44 +0700

    mempercantik page login

--- lib/login.dart
+++ lib/login.dart
@@ -7,9 +7,44 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Login Screen',
-          style: TextStyle(fontSize: 24),
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+              Icon(Icons.lock_outline, size: 100, color: Colors.lightBlue),
+              SizedBox(height: 20),
+              Text(
+                'Login Screen',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Please click login to continue',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  prefixIcon: Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
